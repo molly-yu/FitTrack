19768a8987fb35362c0f2cdcc883150b164bface
molly-yu <55213953+molly-yu@users.noreply.github.com>
2020-05-02 15:37:35 -0400

    Added create users page, connected backend to frontend

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,66 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this); // binding this
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = { // variables
+            username: '',
+        }
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    
+    onSubmit(e){
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username
+        }
+
+        console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user) //post user to server database
+        .then(res => console.log(res.data));
+        
+
+        // window.location = '/'; //back to homepage
+
+        this.setState({
+            username: ''
+        })
+
+    }
+
     render(){
         return(
             <div>
-                <p>Create users</p>
+                <h3>Create new user</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input 
+                            type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
